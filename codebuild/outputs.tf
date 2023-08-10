7144690bf1329f916365653068169943bde5c10e
Mark McLeod <mark.mcleod@digital.homeoffice.gov.uk>
2023-08-10 15:37:49 +0100

    Recofigure modules and branches

--- codebuild/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_codebuild_project_arn" {
-  value       = aws_codebuild_project.this.arn
-  description = "aws codebuild project arn"
-}
--- codepipeline/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "aws_codepipeline_arn" {
-  value       = aws_codepipeline.this.arn
-  description = "aws codepipeline project arn"
-}
